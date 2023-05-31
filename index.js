6c67cdc91632b242e2ba304c0e0da17571600d14
saadaziz0014 <saadaziz0014@gmail.com>
2023-05-31 23:17:56 +0500

    global-vs-nonglobal

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const home = require("/home");
+//global and non-global module aur package
 
-console.log(home);
+//const fs = require("fs"); //not neccessary for variable have same name
+const fswrite = require("fs").writeFileSync;
+console.log(5 + 6); //core module
+
+//fs is non-global need to require it
+
+//fs.writeFileSync("app.txt", "Data in App");
+
+fswrite("code.txt", "Code write");
