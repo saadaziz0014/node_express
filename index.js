4f25135f246ed62cde73320053d3542b7c9045a1
saadaziz0014 <saadaziz0014@gmail.com>
2023-06-01 22:07:22 +0500

    CRUD on file

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-const { log } = require("console");
+const path = require("path");
+const process = require("process");
 const fs = require("fs");
+const { log } = require("console");
 
-fs.readdir(__dirname, (err, item) => {
-  log(item);
-});
+const add = () => {
+  fs.writeFileSync(
+    path.join(__dirname, `public/${process.argv[3]}`),
+    process.argv[4]
+  );
+};
 
-//if err occur it show error
+const remove = () => {
+  fs.unlinkSync(path.join(__dirname, `public/${process.argv[3]}`));
+};
+
+const update = () => {
+  fs.appendFile(
+    path.join(__dirname, `public/${process.argv[3]}`),
+    " Appending ",
+    (err) => {
+      if (err) {
+        log(err);
+      }
+    }
+  );
+};
+
+const read = () => {
+  fs.readFile(
+    path.join(__dirname, `public/${process.argv[3]}`),
+    "utf8",
+    (err, item) => {
+      log(item);
+    }
+  );
+};
+
+const rename = () => {
+  fs.rename(
+    path.join(__dirname, `public/${process.argv[3]}`),
+    path.join(__dirname, `public/${process.argv[4]}`),
+    (err) => {
+      if (err) {
+        log(err);
+      }
+    }
+  );
+};
+
+if (process.argv[2] == "add") {
+  add();
+} else if (process.argv[2] == "remove") {
+  remove();
+} else if (process.argv[2] == "update") {
+  update();
+} else if (process.argv[2] == "read") {
+  read();
+} else if (process.argv[2] == "rename") {
+  rename();
+}
