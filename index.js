c695c8af4b889f1c2bee13eca867b65cf1d0cc9b
saadaziz0014 <saadaziz0014@gmail.com>
2023-06-01 22:41:00 +0500

    send html and json

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ app.get("", (req, res) => {
   res.send("Sent");
 });
 
+app.get("/about", (req, res) => {
+  const data = req.query.id;
+  res.send(data);
+}); //http://localhost:4500/about?id=5
+
+app.get("/file", (req, res) => {
+  res.send("<h1>Saad</h1>");
+});
+
+app.get("/json", (req, res) => {
+  res.send({
+    age: 25,
+    id: 7,
+  });
+});
+
 app.listen(4500);
