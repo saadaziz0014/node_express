8a8b0f20f7ef4e753e4de33effbb3155cdb5ebc0
saadaziz0014 <saadaziz0014@gmail.com>
2023-06-01 21:21:05 +0500

    api_http

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const chalk = require("chalk");
-
-console.log(chalk.blue("Saad") + " and " + chalk.red("Aziz"));
+const http = require("http");
+const d = require("./data.js");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    //res.write("Good");
+    res.write(JSON.stringify(d));
+    res.end();
+  })
+  .listen(4500);
