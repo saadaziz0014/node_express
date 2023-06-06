eb826ecad26bb88d9888d1f8223752408ee6b4ef
saadaziz0014 <saadaziz0014@gmail.com>
2023-06-06 21:22:05 +0500

    os commands

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-import express from "express";
+import os from "os";
 
-import events from "events";
-
-const app = express();
-
-const em = new events.EventEmitter();
-
-let eventC = 0;
-
-em.on("Event Set",(req,res)=>{
-  eventC++;
-})
-
-app.get("",(req,res)=>{
-  res.send("Hi");
-})
-
-app.get("/event",(req,res)=>{
-  em.emit("Event Set","My First Emitter");
-  res.send(`Event Called ${eventC} times`);
-})
-
-app.listen(3000);
+console.log(os.arch());
+console.log(os.freemem()/1024/1024/1024);
+console.log(os.totalmem()/1024/1024/1024);
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.type());
